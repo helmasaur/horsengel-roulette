42698c5b9547f7f7f90f83b5d275ca0fbe17ea56
Helmasaur <contact@helmasaur.fr>
2023-02-23 16:55:20 +0100

    🏷️ Modified type to match discord.js v14 and other
    - MessageEmbed replaced by EmbedBuilder (close #59)
    - some other fixes: prefix variable, async functions, tabs instead of spaces
    - not tested

--- index.d.ts
+++ index.d.ts
@@ -3,11 +3,11 @@ import {
     Guild, 
     GuildMember, 
     TextChannel, 
-    RichEmbed 
+    EmbedBuilder 
 } from 'discord.js';
 
 interface LocaleStrings {
-  [key: string]: string | LocaleStrings;
+    [key: string]: string | LocaleStrings;
 }
 
 export default class HorsengelRoulette<P1 extends GuildMember, P2 extends GuildMember> {
@@ -15,6 +15,7 @@ export default class HorsengelRoulette<P1 extends GuildMember, P2 extends GuildM
     public channel: TextChannel;
     public guild: Guild;
     public players: [P1, P2];
+    public prefix: string;
     public revolver: number[];
     public revolverString: string;
     public strings: LocaleStrings;
@@ -25,22 +26,24 @@ export default class HorsengelRoulette<P1 extends GuildMember, P2 extends GuildM
         msg: Message, 
         player1: P1, 
         player2: P2, 
-        public prefix: string, 
+        prefix: string, 
         language: string
     );
 
     public load(magazine: number, bullets: number): void;
-    public async start(): Promise<Message | GuildMember>;
-    public async game(): Promise<Message | GuildMember>;
-    public async kick(
+    public start(): Promise<Message | GuildMember>;
+    public game(): Promise<Message | GuildMember>;
+    public kick(
         player: P1 | P2, 
         description: string
     ): Promise<Message | GuildMember>;
-    public async sleep(): Promise<void>;
+    public sleep(): Promise<void>;
     public embedRound(
         round: number, 
         description: string, 
         gameOver?: boolean
-    ): RichEmbed;
-    public embedKick(kicked: P1 | P2, reason: string): RichEmbed;
+    ): EmbedBuilder;
+    public embedKick(
+        kicked: P1 | P2,
+        reason: string): EmbedBuilder;
 }
